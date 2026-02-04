259f82206a6ed31fb3be44c28aa199e6f0db6d02
abhya <abhya1559@gmail.com>
2026-02-04 17:45:25 +0530

    initialise project

---
+++ server/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const PORT = 5000;
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello I am server");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
