5f58ddf5cab57b1e30e4abe2f8c26d372b5a4582
abhya <abhya1559@gmail.com>
2026-02-05 10:56:33 +0530

    started working on frontend

--- server/controllers/task.controller.ts
+++ server/controllers/task.controller.ts
@@ -4,6 +4,7 @@ import { AppDataSource } from "../config/db.js";
 import { Task, TaskStatus } from "../entities/Task.js";
 import { AuthRequest } from "../middlewares/auth.middleware.js";
 import { User } from "../entities/User.js";
+import { Like } from "typeorm";
 
 const taskRepo = AppDataSource.getRepository(Task);
 const userRepo = AppDataSource.getRepository(User);
@@ -47,16 +48,42 @@ export const createTask = async (req: AuthRequest, res: Response) => {
 
 export const getTasks = async (req: AuthRequest, res: Response) => {
   try {
-    const userId = req.user?.id;
-    const tasks = await taskRepo.find({
-      where: {
-        user: { id: userId },
-      },
+    const userId = req.user!.id;
+
+    // Query params
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 5;
+    const status = req.query.status as string;
+    const search = req.query.search as string;
+
+    const skip = (page - 1) * limit;
+
+    const where: any = {
+      user: { id: userId },
+    };
+
+    if (status) {
+      where.status = status;
+    }
+
+    if (search) {
+      where.title = Like(`%${search}%`);
+    }
+
+    const [tasks, total] = await taskRepo.findAndCount({
+      where,
+      take: limit,
+      skip,
       order: {
         createdAt: "DESC",
       },
     });
-    return res.json({
+
+    return res.status(200).json({
+      page,
+      limit,
+      total,
+      totalPages: Math.ceil(total / limit),
       tasks,
     });
   } catch (error) {
--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata";
 import express from "express";
-import type { Request, Response } from "express";
 import { AppDataSource } from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import taskRoutes from "./routes/task.routes.js";
+import cors from "cors";
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/auth", authRoutes);
 app.use("/tasks", taskRoutes);
 const start = async () => {
@@ -24,10 +25,6 @@ const start = async () => {
 };
 
 start();
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello I am server");
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
